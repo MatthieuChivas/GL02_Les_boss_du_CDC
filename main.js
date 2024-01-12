a41008544353924633a82f471b99ba0d2fc6ea80
adrienlau <118347287+adrienlau@users.noreply.github.com>
2024-01-12 17:05:01 +0100

    Update main.js adrien

--- main.js
+++ main.js
@@ -173,6 +173,8 @@ class Menu{
     //fonctionnalité pour parcourir la banque de question et crée le QCM
     async parcourirBanqueQuestion() {
         let importQuestions;
+        let bon=0;
+        let i;
         console.clear();
         let path = "./questions_data";
         const getDirectories = fs.readdirSync(__dirname + '/questions_data', { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
@@ -183,11 +185,22 @@ class Menu{
         let choix = await questionAsync("Choix : ");
         path += "/" + choix;
         const getFile = fs.readdirSync(path);
-        console.log("Veuillez choisir un fichier :");
-        getFile.forEach(element => {
-            console.log(element);
-        });
-        choix = await questionAsync("Choix : ");
+        while (bon===0){
+            console.log("Veuillez choisir un fichier :");
+            getFile.forEach(element => {
+                console.log(element);
+            });
+            choix = await questionAsync("Choix : ");
+            bon=0;
+            for (i=0;i<getFile.length;i++){
+                if (choix===getFile[i]){
+                    bon=1;
+                }
+            }
+            if (bon===0){
+                console.log("Nom de fichier incorrect")
+            }
+        }
         path += "/" + choix;
         let importRaw = fs.readFileSync(path, 'utf8');
         let parser = new Parser();
